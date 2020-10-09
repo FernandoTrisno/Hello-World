3ae138e9977d1deebff663891cb09824543e7653
Fernando Trisno Swandi <44857264+FernandoTrisno@users.noreply.github.com>
2020-10-09 08:30:23 +0700

    HomeActivity dan LoginActivity Coba

---
+++ app/src/main/java/com/example/helloworld/HomeActivity.java
@@ -0,0 +1,11 @@
+package com.example.helloworld;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;import android.os.Bundle;
+import  android.widget.FrameLayout;
+
+
+
+public class HomeActivity {
+}
