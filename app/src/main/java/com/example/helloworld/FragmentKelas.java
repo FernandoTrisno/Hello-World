7fda8e1943fdb74cb1b02d47d7004a04864a4951
benediktusaji <44857388+benediktusaji@users.noreply.github.com>
2020-11-05 22:32:22 +0700

    Tugas 6
    -Job Scheduler dan RecyclerView Film di Bioskop

--- app/src/main/java/com/example/helloworld/FragmentKelas.java
+++ app/src/main/java/com/example/helloworld/FragmentKelas.java
@@ -1,6 +1,9 @@
 package com.example.helloworld;
 
 import android.app.NotificationManager;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,14 +16,38 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import static android.content.Context.JOB_SCHEDULER_SERVICE;
+
 public class FragmentKelas extends Fragment {
     private static final String TAG = FragmentKelas.class.getSimpleName();
     Button btnNotifTrigger;
+    Button btnMulai = null;
+    Button btnBerhenti = null;
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnMulai = getView().findViewById(R.id.mulai);
+        btnBerhenti = getView().findViewById(R.id.berhenti);
+        btnMulai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scheduleJob(v);
+            }
+        });
+        btnBerhenti.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                cancelJob(v);
+            }
+        });
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Log.e(TAG, "onCreateView Fragment kelas");
+        Log.e(TAG, "onCreateView Fragment kelas kiri");
         View view = inflater.inflate(R.layout.fragemen_kelas, container, false);
        /* btnNotifTrigger = view.findViewById(R.id.btnLogin);
         btnNotifTrigger.setOnClickListener(new View.OnClickListener() {
@@ -45,4 +72,27 @@ public class FragmentKelas extends Fragment {
         notificationManager.notify(notificationID, builder.build());
 
     }*/
+
+    public void scheduleJob(View v) {
+        ComponentName componentName = new ComponentName(getActivity(), ContohJobScheduler.class);
+        JobInfo info = new JobInfo.Builder(123, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(15 * 60 * 1000)
+                .build();
+        JobScheduler scheduler = (JobScheduler) getActivity().getSystemService(JOB_SCHEDULER_SERVICE);
+        int resultCode = scheduler.schedule(info);
+        if (resultCode == JobScheduler.RESULT_SUCCESS) {
+            Log.d(TAG, "Job scheduled");
+        } else {
+            Log.d(TAG, "Job scheduling failed");
+        }
+    }
+    public void cancelJob(View v) {
+        JobScheduler scheduler = (JobScheduler) getActivity().getSystemService(JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(123);
+        Log.d(TAG, "Job cancelled");
+    }
+
 }
--- app/src/main/java/com/example/helloworld/FragmentPengumuman.java
+++ app/src/main/java/com/example/helloworld/FragmentPengumuman.java
@@ -4,15 +4,33 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class FragmentPengumuman extends Fragment {
 
+    RecyclerView recyclerView;
+    String film[], desc[];
+    int foto[] = {R.drawable.aac,R.drawable.aadc,R.drawable.sok};
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = getView().findViewById(R.id.recycleview);
+        MyAdapterClass myAdapterClass = new MyAdapterClass(getActivity(), film,desc,foto);
+        recyclerView.setAdapter(myAdapterClass);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        film = getResources().getStringArray(R.array.film);
+        desc = getResources().getStringArray(R.array.film_desc);
         return inflater.inflate(R.layout.fragment_pengumuman,container,false);
     }
 }
