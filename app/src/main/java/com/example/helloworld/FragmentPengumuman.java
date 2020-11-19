7d393dafbb1d4d252f49492f64f80f52e2f9638e
Fernando Trisno Swandi <44857264+FernandoTrisno@users.noreply.github.com>
2020-11-20 00:01:11 +0700

    Tugas 8

--- app/src/main/java/com/example/helloworld/FragmentPengumuman.java
+++ app/src/main/java/com/example/helloworld/FragmentPengumuman.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class FragmentPengumuman extends Fragment {
 
